89f67ff7b1b5d6eb2a72c98f289d9a4be13cba1d
natalia <ailatangh@gmail.com>
2017-06-25 15:23:31 +0200

    created function created html code

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
 'use strict';
+// Created HTML code
+function createdHtml(){
+  var contentElement = document.querySelector('.container');
 
+  var divUsername = document.createElement("div");
+  divUsername.setAttribute('class', 'username');
+
+  var divContent = document.createElement('div');
+  divContent.setAttribute('class','content');
+
+  var imgIcon = document.createElement('img');
+  imgIcon.setAttribute('class','icon');
+  imgIcon.src = 'githubB.png';
+
+  var divUsername = document.createElement('div');
+  divUsername.setAttribute('class','username');
+
+  var divInfo = document.createElement('div');
+  divInfo.setAttribute('class','info');
+
+  var h3Name = document.createElement('h3');
+  h3Name.innerHTML = 'Nombre:';
+
+  var spanBoxname = document.createElement('span');
+  spanBoxname.setAttribute('id','boxName');
+
+  var brElement = document.createElement('br');
+
+  var imgName = document.createElement('img');
+  imgName.setAttribute('id','imgName');
+
+  var h3Repos = document.createElement('h3');
+  h3Repos.innerHTML = 'Número de repositorios:';
+
+  var spanBoxrepos = document.createElement('span');
+  spanBoxrepos.setAttribute('id','boxRepos');
+
+  contentElement.appendChild(divUsername);
+  divUsername.appendChild(divContent);
+  divContent.appendChild(imgIcon);
+  divContent.appendChild(divInfo);
+  divInfo.appendChild(h3Name);
+  divInfo.appendChild(spanBoxname);
+  divInfo.appendChild(brElement);
+  divInfo.appendChild(imgName);
+  divInfo.appendChild(h3Repos);
+}
+
+createdHtml();
+
+// Function Consul username
 var SearchElement = document.getElementById('seach');
 var boxName = document.getElementById('boxName');
 var imgName = document.getElementById('imgName');
